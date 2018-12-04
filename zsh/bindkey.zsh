a073d8241c453eef88abc9104303388956462d31
OneTone <OneTone@gmail.com>
2018-12-04 23:48:26 +0800

    zshrc: more key bindings
    Signed-off-by: OneTone <OneTone@gmail.com>

--- zsh/bindkey.zsh
+++ zsh/bindkey.zsh
@@ -15,3 +15,10 @@ fi
 bindkey '^U' backward-kill-line
 bindkey '^[k' kill-whole-line
 bindkey '^[l' down-case-word
+
+bindkey '^X^?' where-is
+bindkey '^X\^' vi-first-non-blank
+bindkey '^X^F' vi-find-next-char
+bindkey '^X^T' vi-find-next-char-skip
+bindkey '^XF' vi-find-prev-char
+bindkey '^XT' vi-find-prev-char-skip
