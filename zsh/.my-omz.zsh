1cce828f879d621771f0648bc34c26922fef42bd
OneTone <OneTone@gmail.com>
2019-04-26 23:25:04 +0800

    zshrc: refactor .my-omz.zsh to source local .zshrc.plug
    Signed-off-by: OneTone <OneTone@gmail.com>

--- zsh/.my-omz.zsh
+++ zsh/.my-omz.zsh
@@ -2,44 +2,32 @@
 
 _my_omz_start="$(print -P '%D{%s.%.}' 2>/dev/null)"
 
-ZSH_CUSTOM="$HOME/.dotfiles/zsh"
+ZSH_CUSTOM="${${(%):-%x}:A:h}"
 
-ZSH_THEME='onetone'
+plugins=()
+[ ${(%):-%(!.0.1)} = 0 ] || plugins+=('sudo')
 
-_add_cmd_plugin() {
-  local _cmd
-  for _cmd; do
-    which "$_cmd" >/dev/null 2>&1 && plugins+=("$_cmd")
-  done
-}
+: "${_ZSHRC_PLUG:=$HOME/.zshrc.plug}"
+if [ -f "$_ZSHRC_PLUG" ]; then
+  _add_omz_plugin() {
+    local arg cmd plug
+    for arg; do
+      cmd="${arg%%:*}"
+      plug="${arg##*:}"
+      if [ "$cmd" ]; then
+        : "${plug:=$cmd}"
+        which "$cmd" >/dev/null 2>&1 && plugins+=("$plug")
+      else
+        [ "$plug" ] && plugins+=("$plug")
+      fi
+    done
+  }
 
-case "$OSTYPE" in
-  darwin*)
-    plugins=('osx')
-    _add_cmd_plugin 'brew'
-    ;;
-  *) plugins=() ;;
-esac
+  source "$_ZSHRC_PLUG"
+  unset -f _add_omz_plugin
+fi
 
-[ "$(id -u)" = 0 ] || plugins+=('sudo')
-
-plugins+=(
-  'colored-man-pages'
-  'command-not-found'
-  'encode64'
-  'extract'
-  'history'
-  'jsontools'
-  'urltools'
-  'web-search'
-  'zsh_reload'
-  'zsh-navigation-tools'
-)
-
-_add_cmd_plugin 'adb' 'docker' 'git' 'nmap' 'pip' 'repo' 'vagrant'
-unset -f _add_cmd_plugin
-
-. "$ZSH/oh-my-zsh.sh"
+source "$ZSH/oh-my-zsh.sh"
 
 _my_omz_end="$(print -P '%D{%s.%.}' 2>/dev/null)"
 printf '\e[30;1mmy-omz started in %.3fs.\e[m\n' "$((_my_omz_end - _my_omz_start))"
