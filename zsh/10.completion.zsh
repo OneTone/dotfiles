4768f047d95273c233e401f3056a50088b449185
OneTone <OneTone@gmail.com>
2019-04-30 15:47:47 +0800

    set more completion options
    Signed-off-by: OneTone <OneTone@gmail.com>

--- zsh/10.completion.zsh
+++ zsh/10.completion.zsh
@@ -1,4 +1,8 @@
 # vim: ft=zsh ts=2 sw=2 sts=2 et
 
+setopt globcomplete
+setopt listpacked
+setopt listrowsfirst
+
 zstyle -d ':completion:*' users
 zstyle ':completion:*' rehash true
--- zsh/10.glob.zsh
+++ zsh/10.glob.zsh
@@ -1,5 +1,4 @@
 # vim: ft=zsh ts=2 sw=2 sts=2 et
 
 setopt extendedglob
-setopt globcomplete
 setopt shwordsplit
