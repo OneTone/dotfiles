7870bc832f703832e4305526e24b663578ad70c4
OneTone <OneTone@gmail.com>
2020-10-08 01:19:53 +0800

    zsh: empty WORDCHARS; define widgets with local WORDCHARS
    Signed-off-by: OneTone <OneTone@gmail.com>

--- zsh/20-bindkey.zsh
+++ zsh/20-bindkey.zsh
@@ -1,18 +1,46 @@
 # vim: ft=zsh ts=2 sw=2 sts=2 et
 
-unix-word-rubout() {
-  local WORDCHARS=$'!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
-  zle backward-kill-word
+WORDCHARS=''
+
+local_wordchars_widget() {
+  eval "$2() {
+    local WORDCHARS='${3//'/'\"'\"'}'
+    zle $1
+  }"
+  zle -N "$2"
+  if [ "$4" ]; then
+    bindkey "$4" "$2"
+  fi
 }
 
-zle -N unix-word-rubout
-bindkey '^W' unix-word-rubout
+local_wordchars_widget backward-kill-word unix-word-rubout \
+$'!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~' '^W'
+
+-._() {
+  local_wordchars_widget "$1" "$1-._" '-._' "$2"
+}
+
+-._ backward-kill-word '^[^H'
+-._ backward-word '^[B'
+-._ capitalize-word '^[C'
+-._ copy-prev-word '^[M'
+-._ down-case-word '^[L'
+-._ forward-word '^[F'
+-._ insert-last-word '^[_'
+-._ kill-word '^[D'
+-._ spell-word '^[S'
+-._ transpose-words '^[T'
+-._ up-case-word '^[U'
+
+unset -f -- -._ local_wordchars_widget
 
 if which sudo-command-line >/dev/null 2>&1; then
+  bindkey '^[E' sudo-command-line
   bindkey '^[e' sudo-command-line
 fi
 
 bindkey '^U' backward-kill-line
+bindkey '^[K' kill-whole-line
 bindkey '^[k' kill-whole-line
 bindkey '^[l' down-case-word
 
